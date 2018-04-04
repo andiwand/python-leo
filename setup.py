74e81a1e8be4b235228b6662af79af2f250225e2
Tobias Rueetschi <tr@brief.li>
2018-04-04 20:38:42 +0200

    Fix license issue

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description='Leo Dictionary',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
